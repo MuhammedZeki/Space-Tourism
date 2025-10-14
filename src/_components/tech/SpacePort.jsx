634aef7a7def3ca0b10ff2e37299959c39e7e296
MuhammedZeki <muhammed.codes@gmail.com>
2025-10-14 13:35:24 +0300

    prod

--- src/_components/tech/SpacePort.jsx
+++ src/_components/tech/SpacePort.jsx
@@ -1,7 +1,7 @@
-import TechImg from "../../assets/technology/image-launch-vehicle-portrait.jpg";
-import TechLand from "../../assets/technology/image-launch-vehicle-landscape.jpg";
+import TechImg from "../../assets/technology/image-spaceport-portrait.jpg";
+import TechLand from "../../assets/technology/image-spaceport-landscape.jpg";
 import "../../css/technology/TechAllPage.css";
-const LaunchVehicle = ({ tabs, setTabs }) => {
+const SpacePort = ({ tabs, setTabs }) => {
   return (
     <div className="tech-inform">
       <div className="left-bar">
@@ -29,28 +29,26 @@ const LaunchVehicle = ({ tabs, setTabs }) => {
           <div className="right-content">
             <div className="content-titles">
               <span>THE TERMINOLOGY…</span>
-              <p>LAUNCH VEHICLE</p>
+              <p>SPACEPORT</p>
             </div>
             <p className="content-text">
-              A launch vehicle or carrier rocket is a rocket-propelled vehicle
-              used to carry a payload from Earth's surface to space, usually to
-              Earth orbit or beyond. Our WEB-X carrier rocket is the most
-              powerful in operation. Standing 150 metres tall, it's quite an
-              awe-inspiring sight on the launch pad!
+              A spaceport or cosmodrome is a site for launching -or receiving-
+              spacecraft, by analogy to the seaport for ships or airport for
+              aircraft. Based in the famous Cape Canaveral, our spaceport is
+              ideally situated to take advantage of the Earth’s rotation for
+              launch.
             </p>
           </div>
         </div>
       </div>
       <div className="right-bar">
-        <div className="right-bar">
-          <picture>
-            <source media="(max-width:900px)" srcSet={TechLand} />
-            <img src={TechImg} alt="vehicle" />
-          </picture>
-        </div>
+        <picture>
+          <source media="(max-width:900px)" srcSet={TechLand} />
+          <img src={TechImg} alt="vehicle" />
+        </picture>
       </div>
     </div>
   );
 };
 
-export default LaunchVehicle;
+export default SpacePort;
--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Header from "../_components/Header";
 import "../css/technology/TechnologyPage.css";
 import LaunchVehicle from "../_components/tech/LaunchVehicle";
+import SpacePort from "../_components/tech/SpacePort";
+import SpaceCapsule from "../_components/tech/SpaceCapsule";
 const Technology = () => {
   const [tabs, setTabs] = useState(0);
 
@@ -14,6 +16,7 @@ const Technology = () => {
           <p>SPACE LAUNCH 101</p>
         </div>
         {tabs === 0 && <LaunchVehicle tabs={tabs} setTabs={setTabs} />}
+        {tabs === 1 && <SpacePort tabs={tabs} setTabs={setTabs} />}
       </div>
     </div>
   );
